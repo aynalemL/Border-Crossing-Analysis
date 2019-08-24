033afcf9ed25bd43c4ad6ac3576ddc7a6e1f68e8
Aynalem Lenjiso <aynalem.lenjiso@gmail.com>
2019-08-24 11:10:05 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
 # python3.7 ./src/border_analytics.py ./input/Border_Crossing_Entry_data.csv ./output/report.csv
-ls -l -d $TEST_OUTPUT_PATH/*
+ls  $TEST_OUTPUT_PATH/../
 java -classpath /home/ubuntu/cc-grader/20A/apps/aynalemL_github/target/production/*  report.MainApplication /home/ubuntu/cc-grader/20A/apps/aynalemL_github/temp/input/Border_Crossing_Entry_Data.csv /home/ubuntu/cc-grader/20A/apps/aynalemL_github/temp/output/report.csv
  
